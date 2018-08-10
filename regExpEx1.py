419fee0af9853f310b4b17d4cf6fc695383b3a82
Fabiano Girardi <girardi.fabiano@gmail.com>
2018-08-10 07:21:00 +0000

    Example ExpReg

---
+++ regExpEx1.py
@@ -0,0 +1,12 @@
+import re
+
+#r = raw string cru nao escapa nada
+regexp = r'(\d\d)(\w)'
+match = '12a563a2223a'
+
+result = re.search(regexp, match)
+print result.group()
+
+results = re.finditer(regexp, match)
+for result in results:
+	print("%s | %s | %s | [%s-%s]" % (result.group(0), result.group(1), result.group(2), result.start(), result.end()))
