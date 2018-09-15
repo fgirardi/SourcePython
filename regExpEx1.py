7ffecaa2fee4e5c9819bbfa23a8e2e398ee7d6e0
Fabiano Girardi <girardi.fabiano@gmail.com>
2018-09-15 12:21:19 +0000

    Example regExp

--- regExpEx1.py
+++ regExpEx1.py
@@ -8,5 +8,6 @@ result = re.search(regexp, match)
 print result.group()
 
 results = re.finditer(regexp, match)
+
 for result in results:
 	print("%s | %s | %s | [%s-%s]" % (result.group(0), result.group(1), result.group(2), result.start(), result.end()))
