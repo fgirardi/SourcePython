0c2fa78a8e2ebee163f2980ca3349ffe970da2ae
Fabiano Girardi <girardi.fabiano@gmail.com>
2018-10-03 15:49:38 -0300

    WIPI

--- Socket/Server.py
+++ Socket/Server.py
@@ -13,3 +13,20 @@ def CreateSocket():
 	except socket.error as msg:
 		print("Error CreateSocket. {0}".format(str(msg)))
 
+
+def BindSocket():
+
+	try
+
+		global host
+		global port
+		global s
+
+		print("Binding the Port {0}".format(str(host)))
+
+		s.bind(host,port)
+		s.listen(5)
+	
+	except socket.error as msg:
+		print("Error BindSocket. {0}\nRetriying...".format(str(host)))
+		BindSocket()
