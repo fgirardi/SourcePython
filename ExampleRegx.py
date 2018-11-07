2326b3781807859d849559cb62830b4b0fd3a4bc
Fabiano Girardi <girardi.fabiano@gmail.com>
2018-11-07 11:20:54 -0200

    Delete ExampleRegx.py

--- ExampleRegx.py
+++
@@ -1,9 +0,0 @@
-import re
-
-match = re.search('([a-z]+)\s([a-z])', 'Guilherme Orlando Girardi')
-if match:
-	print("[{0}]".format(match.group(0)))
-	print("[{0}]".format(match.group(1)))
-	print("[{0}]".format(match.group(2)))
-else:
-	print('sad')
