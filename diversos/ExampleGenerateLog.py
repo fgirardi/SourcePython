17db07dc90e72698695b76db2209996f70aec5d6
Fabiano Girardi <girardi.fabiano@gmail.com>
2018-12-17 19:35:18 -0200

    New Changed

--- diversos/ExampleGenerateLog.py
+++ diversos/ExampleGenerateLog.py
@@ -1,23 +1,10 @@
 import logging
-logging.basicConfig(level=logging.INFO, filename = "fabiano.log")
-logging.basicConfig(level=logging.DEBUG,filename = "fabiano.log")
-logger = logging.getLogger(__name__)
-
-# create a file handler
-handler = logging.FileHandler('fabiano.log')
-handler.setLevel(logging.INFO)
-
-# create a logging format
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-
-# add the handlers to the logger
-logger.addHandler(handler)
-
-logger.info('Start reading database')
-# read database here
-records = {'john': 55, 'tom': 66}
-logger.info('Records: %s', records)
-logger.info('Updating records ...')
-# update records here
-logger.info('Finish updating records')
+import time
+ 
+logging.basicConfig(level = logging.INFO, filename = time.strftime("my-%Y-%m-%d.log"))
+  
+logging.debug("debug") 
+logging.info("info") 
+logging.warning("warning") 
+logging.error("error")
+logging.critical("critical")
