3f4bc04f45f42fe48d68e2ca471c353eee7b43a3
Fabiano Girardi <girardi.fabiano@gmail.com>
2018-12-17 19:15:13 -0200

    New changes

--- diversos/ExampleGenerateLog.py
+++ diversos/ExampleGenerateLog.py
@@ -1,11 +1,23 @@
 import logging
 logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
+
+# create a file handler
+handler = logging.FileHandler('fabiano.log')
+handler.setLevel(logging.INFO)
+
+# create a logging format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(handler)
 
 logger.info('Start reading database')
 # read database here
 records = {'john': 55, 'tom': 66}
-logger.debug('Records: %s', records)
+logger.info('Records: %s', records)
 logger.info('Updating records ...')
 # update records here
 logger.info('Finish updating records')
